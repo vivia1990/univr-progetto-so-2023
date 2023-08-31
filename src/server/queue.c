0e3055f952aabdc7ac3ab7ed1168a1d7e61538d9
vivia1990 <donvivia@gmail.com>
2023-08-31 15:55:33 +0200

    refactor(queue): migliorata implementazione

--- src/server/queue.c
+++ src/server/queue.c
@@ -1,6 +1,6 @@
-#include "queue_api.h"
 #include "log.h"
 #include "messages.h"
+#include "queue_api.h"
 #include "server.h"
 #include <asm-generic/errno-base.h>
 #include <errno.h>
@@ -8,24 +8,7 @@
 #include <string.h>
 #include <sys/msg.h>
 
-static int32_t
-queue_send(int32_t qId, const struct Payload *pl)
-{
-    ssize_t status = 0;
-    do {
-        errno = 0;
-        status = msgsnd(qId, pl, MSG_SIZE(Payload), IPC_NOWAIT);
-    } while (errno == EINTR);
-
-    if (status < 0) {
-        LOG_ERROR("Errore invio payload queue_api", "");
-        return status;
-    }
-
-    return pl->mtype;
-}
-
-static int32_t
+int32_t
 queue_recive(int32_t qId, struct Payload *pl, uint32_t mType)
 {
     ssize_t status = 0;
@@ -45,56 +28,4 @@ queue_recive(int32_t qId, struct Payload *pl, uint32_t mType)
     }
 
     return pl->mtype;
-}
-
-int32_t
-queue_send_game(int32_t qId, const void *msg, size_t msgSize,
-                int32_t messageType)
-{
-    struct Payload pl = {.mtype = messageType};
-    memcpy(pl.payload, msg, msgSize);
-
-    return queue_send(qId, &pl);
-}
-
-int32_t
-queue_recive_game(int32_t qId, void *msg, size_t msgSize, int32_t messageType)
-{
-    struct Payload pl = {};
-    ssize_t status = queue_recive(qId, &pl, messageType);
-    memcpy(msg, pl.payload, msgSize);
-
-    return status;
-}
-
-int32_t
-queue_send_connection(int32_t qId, const void *msg, size_t msgSize,
-                      uint32_t msgType)
-{
-    struct Payload pl = {};
-    pl.mtype = msgType;
-    memcpy(pl.payload, msg, msgSize);
-
-    return queue_send(qId, &pl);
-}
-
-int32_t
-queue_recive_connection(int32_t qId, void *msg, size_t msgSize,
-                        uint32_t msgType)
-{
-    struct Payload pl = {};
-    ssize_t status = queue_recive(qId, &pl, msgType);
-    memcpy(msg, pl.payload, msgSize);
-
-    return status;
-}
-
-int32_t
-queue_send_error(int32_t qId, const void *msg, size_t msgSize)
-{
-    struct Payload pl = {};
-    pl.mtype = MSG_ERROR;
-    memcpy(pl.payload, msg, msgSize);
-
-    return queue_send(qId, &pl);
 }
